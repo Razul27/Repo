5d231e1b26f1c17619f087ae1f83279d05fa4957
Razul27 <mohamedrazul916@gmail.com>
2022-07-23 08:16:15 +0530

    New Code

---
+++ src/org/students/Scanner.java
@@ -0,0 +1,28 @@
+package org.students;
+
+public class Scanner {
+	public static void main(String[] args) {
+		short id=9315;
+		System.out.println("Student Id is "+id);
+		
+		String Name="Razul";
+		System.out.println("Student Name is "+Name);
+		
+		String Email="razul744@gmail.com";
+		System.out.println("Student Email Id is "+Email);
+		
+		long phoneNo=9566055053l;
+		System.out.println("Student PhoneNo is "+phoneNo);
+		
+		String dept="Computer Science";
+		System.out.println("Student Dept is "+dept);
+		
+		char gender='m';
+		System.out.println("Student gender is "+gender);
+		
+		String city="Chennai";
+		System.out.println("Student City is "+city);
+		
+	}
+
+}
